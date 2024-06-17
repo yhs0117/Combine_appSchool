62b88caf9a739f98dc9c7e12f5a2c04ba3d93530
Hyunseo Yook <hhh3332@naver.com>
2024-06-17 17:11:11 +0900

    Combine

--- ListView/ListView/BooksListView.swift
+++ ListView/ListView/BooksListView.swift
@@ -5,18 +5,66 @@
 //  Created by 육현서 on 6/17/24.
 //
 
+
 import SwiftUI
+import Combine
+
+private extension String {
+  func matches(_ searchTerm: String) -> Bool {
+    self.range(of: searchTerm, options: .caseInsensitive) != nil
+  }
+}
 
 private class BooksViewModel: ObservableObject {
     @Published var books: [Book] = Book.samples
+    @Published var fetching = false
+    @Published var searchTerm: String = ""
+    
+    @Published var filteredBooks: [Book] = [Book]()
+
+    init() {
+        Publishers.CombineLatest($books, $searchTerm)
+            .map { books, searchTerm in
+              books.filter { book in
+                searchTerm.isEmpty ? true : (book.title.matches(searchTerm) || book.author.matches(searchTerm))
+              }
+            }
+            .assign(to: &$filteredBooks)
+    }
+    
+    @MainActor
+    func fetchData() async {
+        fetching = true
+        books.removeAll()
+        do {
+            try await Task.sleep(for: .seconds(2))
+        } catch {}
+        books = Book.samples
+        fetching = false
+    }
 }
 
 struct BooksListView: View {
     @StateObject fileprivate var viewModel = BooksViewModel()
     
     var body: some View {
-        List(viewModel.books) { book in
+        List(viewModel.filteredBooks) { book in
             BookRowView(book: book)
+        }
+        .searchable(text: $viewModel.searchTerm)
+        .autocapitalization(.none)
+        .refreshable {
+            await viewModel.fetchData()
+        }
+        .overlay {
+            if viewModel.fetching {
+                ProgressView("Fetching data, please wait...")
+                    .progressViewStyle(CircularProgressViewStyle())
+            }
+        }
+        .animation(.default, value: viewModel.books)
+        .task {
+            await viewModel.fetchData()
         }
     }
 }
