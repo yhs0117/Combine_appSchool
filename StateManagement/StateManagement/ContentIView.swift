1c7cba9d2a0f7a82b9673b72528d3bf4fca71470
Hyunseo Yook <hhh3332@naver.com>
2024-06-17 13:36:19 +0900

    StateObject

---
+++ StateManagement/StateManagement/ContentIView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentIView.swift
+//  StateManagement
+//
+//  Created by 육현서 on 6/17/24.
+//
+
+
+import SwiftUI
+
+class Counter: ObservableObject {
+    @Published var count = 0
+}
+
+struct StateStepper: View {
+    @StateObject var stateCounter = Counter()
+    
+    var body: some View {
+        Section(header: Text("@StateObject")) {
+            Stepper("Counter: \(stateCounter.count)", value: $stateCounter.count)
+        }
+    }
+}
+
+struct ContentView: View {
+    @State var color: Color = Color.accentColor
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            StateStepper()
+            ColorPicker("Pick a color", selection: $color)
+        }
+        .foregroundStyle(color)
+    }
+}
+
+#Preview {
+    ContentView()
+}
