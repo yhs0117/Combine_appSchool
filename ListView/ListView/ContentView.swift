28ce673a0c23a5507ab607a07364ffbc0f2022d2
Hyunseo Yook <hhh3332@naver.com>
2024-06-17 14:48:10 +0900

    Simple List

--- ListView/ListView/ContentView.swift
+++ ListView/ListView/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var number: Int = 42
+    @State var degrees: Double = 37.5
+    @State var toggle = true
+    @State var name = "Peter"
+    @State var secret = "s3cr3t!"
+    var fruits = ["Apples", "Bananas", "Mangoes"]
+    
+    @State var fruit = "Mangoes"
     var body: some View {
         List {
-            Text("Hello, World !")
-            Text("Hello, SwiftUI !")
+            Text("Hello, world!")
+            Label("The answer", systemImage: "42.circle")
+            Slider(value: $degrees, in: 0...50) {
+                Text("\(degrees)")
+            } minimumValueLabel: {
+                Text("min")
+            } maximumValueLabel: {
+                Text("max")
+            }
+            Stepper(value: $number, in: 0...100) {
+                Text("\(number)")
+            }
+            Toggle(isOn: $toggle) {
+                Text("Checked")
+            }
+            TextField("Name", text: $name)
+            SecureField("Secret", text: $secret)
+            ProgressView(value: 0.3)
+            Picker(selection: $fruit, label: Text("Pick your favourite fruit")) {
+                ForEach(fruits, id: \.self) { fruit in
+                    Text(fruit)
+                }
+            }
         }
-        .listStyle(.plain)
-    }
-}
+    }}
 
 #Preview {
     ContentView()
