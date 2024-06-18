9bf56d4d303a052b660aacf5161ac85d25ef2c53
Hyunseo Yook <hhh3332@naver.com>
2024-06-18 10:05:10 +0900

    EditViewModel 추가

--- BookShelf/BookShelf/BookEditView.swift
+++ BookShelf/BookShelf/BookEditView.swift
@@ -7,20 +7,65 @@
 
 import SwiftUI
 
+
+func checkISBN(isbn: String) -> Bool {
+  guard !isbn.isEmpty else {
+    return false
+  }
+  
+  let sum = isbn
+    .compactMap { $0.wholeNumberValue }
+    .enumerated()
+    .map { $0.offset & 1 == 1 ? 3 * $0.element : $0.element }
+    .reduce(0, +)
+  
+  return sum % 10 == 0
+}
+
+class BookEditViewModel: ObservableObject {
+    @Published var book: Book
+    var isISBNValid: Bool {
+        checkISBN(isbn: book.isbn)
+    }
+    
+    init(book: Book) {
+        self.book = book
+    }
+    
+}
+
 struct BookEditView: View {
-    @Binding var book: Book
+    @ObservedObject var bookEditViewModel: BookEditViewModel
+    
+    init(book: Book) {
+        self.bookEditViewModel = BookEditViewModel(book: book)
+    }
+    
     var body: some View {
         Form {
-            TextField("Book title", text: $book.title)
-            Image(book.largeCoverImageName)
-            TextField("Author", text: $book.author)
-            TextField("Pages", value: $book.pages,
+            TextField("Book title", text: $bookEditViewModel.book.title)
+            Image(bookEditViewModel.book.largeCoverImageName)
+            TextField("Author", text: $bookEditViewModel.book.author)
+            VStack(alignment: .leading) {
+                if !bookEditViewModel.isISBNValid {
+                    Text("ISBN is invalid")
+                        .font(.caption)
+                        .foregroundStyle(Color.red)
+                }
+                TextField("ISBN", text: $bookEditViewModel.book.isbn)
+            }
+            TextField("Pages", value: $bookEditViewModel.book.pages,
                       formatter: NumberFormatter())
-            Toggle("Read", isOn: $book.isRead)
+            Toggle("Read", isOn: $bookEditViewModel.book.isRead)
         }
+        .onChange(of: bookEditViewModel.book.isbn) {
+            print("TEST \(bookEditViewModel.isISBNValid)")
+            print("isbn: \(bookEditViewModel.book.isbn)")
+        }
+
     }
 }
 
 #Preview {
-    BookEditView(book: .constant(Book.sampleBooks[0]))
+    BookEditView(book: Book.sampleBooks[0])
 }
