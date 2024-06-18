6029c82dfc04dedb60ce03974c764aebc4e7b097
Hyunseo Yook <hhh3332@naver.com>
2024-06-18 09:21:09 +0900

    Edit Form

--- BookShelf/BookShelf/BookDetailView.swift
+++ BookShelf/BookShelf/BookDetailView.swift
@@ -18,15 +18,18 @@ struct BookDetailView: View {
                 .scaledToFit()
                 .shadow(radius: 10)
                 .padding()
+            Button(action: { showEditBookView.toggle() }) {
+                Label("Edit", systemImage: "pencil")
+            }
             Label(book.author, systemImage: "person.crop.rectangle")
             Label("ISBN: \(book.isbn)", systemImage: "number")
             Label("\(book.pages) pages", systemImage: "book")
             Toggle("Read", isOn: .constant(book.isRead))
-            Button(action: { showEditBookView.toggle() }) {
-                Label("Edit", systemImage: "pencil")
-            }
         }
         .navigationTitle(book.title)
+        .sheet(isPresented: $showEditBookView) {
+            BookEditView(book: $book)
+        }
     }
 }
 
---
+++ BookShelf/BookShelf/BookEditView.swift
@@ -0,0 +1,26 @@
+//
+//  BookEditView.swift
+//  BookShelf
+//
+//  Created by 육현서 on 6/18/24.
+//
+
+import SwiftUI
+
+struct BookEditView: View {
+    @Binding var book: Book
+    var body: some View {
+        Form {
+            TextField("Book title", text: $book.title)
+            Image(book.largeCoverImageName)
+            TextField("Author", text: $book.author)
+            TextField("Pages", value: $book.pages,
+                      formatter: NumberFormatter())
+            Toggle("Read", isOn: $book.isRead)
+        }
+    }
+}
+
+#Preview {
+    BookEditView(book: .constant(Book.sampleBooks[0]))
+}
